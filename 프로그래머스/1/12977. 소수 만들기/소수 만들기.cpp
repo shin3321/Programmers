62c26277a8a3e573fbd2d22f2c7e76ed8a76f860
shin3321 <134493990+shin3321@users.noreply.github.com>
2026-03-16 12:42:20 +0900

    [level 1] Title: 소수 만들기, Time: 25.04 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.cpp
@@ -0,0 +1,37 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+bool isPrime(int prime)
+{
+    for(int i = 2; i *i <= prime; ++i)
+    {
+        if(prime % i == 0)
+            return false;
+    }
+    return true;
+}
+    
+
+int solution(vector<int> nums) {
+    int answer = 0;
+    int sum = 0;
+     for(int i = 0 ; i < nums.size() -2; ++ i)
+     {
+         for(int j = i+1; j < nums.size() -1; ++j)
+         {
+             for(int k = j+1; k < nums.size(); ++k)
+             {
+                 sum = nums[i] + nums[j] + nums[k];
+                     cout << sum << endl;
+                 if(isPrime(sum))
+                 {
+                     ++answer;
+                 }
+             }
+         }
+     }
+    return answer;
+}
